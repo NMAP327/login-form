70e52f129ea3432b949ec901177c8dc82543caf8
NMAP327 <nadyamelania27@gmail.com>
2023-04-17 12:07:39 +0700

    first commit

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+  
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"> 
+   
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+</head>
+<body>
+    <a href="/auth/login" class="btn btn-info" role="button">Logout</a>
+    <a href="/auth/password" class="btn btn-info" role="button">Reset Password</a>
+  <div class="px-4 py-5 my-5 text-center">
+    <img class="d-block mx-auto mb-4" src="logo.jpg" alt="" width="150" height="100">
+    <h1 class="display-5 fw-bold">hello </h1>
+      </div>
+    </div>
+  </div>
+</body>
+</html>
+    
+<main>
+  
