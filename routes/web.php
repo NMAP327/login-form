f58cc6a2888c2a1e247e1c8996ece061d45f41c7
NMAP327 <nadyamelania27@gmail.com>
2023-04-17 08:20:29 +0700

    sa

--- routes/web.php
+++ routes/web.php
@@ -27,15 +27,6 @@ Route::get('/auth/logout',[AuthController::class, 'logout'])->name('auth.logout'
 route::get('/auth/reset_password',[AuthController::class, 'reset_password'])->name('auth.reset_password');
 
 
-
-
-#Route::group(['middleware'=>['AuthCheck']], function(){
-    Route::get('/auth/login',[AuthController::class, 'login'])->name('auth.login');
-    Route::get('/auth/register',[AuthController::class, 'register'])->name('auth.register');
-    Route::get('/auth/password',[AuthController::class, 'password'])->name('auth.password');
-
-    #Route::get('/admin/dashboard',[MainController::class, 'dashboard']);
-    #Route::get('/admin/settings',[MainController::class,'settings']);
-    #Route::get('/admin/profile',[MainController::class,'profile']);
-    #Route::get('/admin/staff',[MainController::class,'staff']);
-#});
+Route::get('/auth/login',[AuthController::class, 'login'])->name('auth.login');
+Route::get('/auth/register',[AuthController::class, 'register'])->name('auth.register');
+Route::get('/auth/password',[AuthController::class, 'password'])->name('auth.password');
